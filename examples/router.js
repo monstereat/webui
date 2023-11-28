0039a9e77dc7065b7c4262ada126476e68f53395
“monstereat  git config --global user.email ”532160108@qq.com <532160108@qq.com>
2023-11-28 16:40:00 +0800

    feat：添加示例

--- examples/router.js
+++ examples/router.js
@@ -1,31 +1,141 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import navConfig from './nav.config';
+
+const LOAD_MAP = {
+    // 'zh-CN': name => {
+    //     return r => require.ensure([], () =>
+    //         r(require(`./pages/zh-CN/${name}.vue`)),
+    //         'zh-CN');
+    // },
+    // 'en-US': name => {
+    //     return r => require.ensure([], () =>
+    //         r(require(`./pages/en-US/${name}.vue`)),
+    //         'en-US');
+    // },
+    // 'es': name => {
+    //     return r => require.ensure([], () =>
+    //         r(require(`./pages/es/${name}.vue`)),
+    //         'es');
+    // },
+    // 'fr-FR': name => {
+    //     return r => require.ensure([], () =>
+    //         r(require(`./pages/fr-FR/${name}.vue`)),
+    //         'fr-FR');
+    // }
+};
+
+const load = function (lang, path) {
+    return LOAD_MAP[lang](path);
+};
+
+const LOAD_DOCS_MAP = {
+    'zh-CN': path => {
+        return r => require.ensure([], () =>
+            r(require(`./docs/zh-CN${path}.md`)),
+            'zh-CN');
+    },
+    'en-US': path => {
+        return r => require.ensure([], () =>
+            r(require(`./docs/en-US${path}.md`)),
+            'en-US');
+    },
+    'es': path => {
+        return r => require.ensure([], () =>
+            r(require(`./docs/es${path}.md`)),
+            'es');
+    },
+    'fr-FR': path => {
+        return r => require.ensure([], () =>
+            r(require(`./docs/fr-FR${path}.md`)),
+            'fr-FR');
+    }
+};
+
+const registerRoute = (navConfig) => {
+    let route = [];
+    Object.keys(navConfig).forEach((lang, index) => {
+        let navs = navConfig[lang];
+        route.push({
+            path: `/${ lang }/component`,
+            redirect: `/${ lang }/component/installation`,
+            // component: load(lang, 'component'),
+            children: []
+        });
+        navs.forEach(nav => {
+            if (nav.href) return;
+            if (nav.groups) {
+                nav.groups.forEach(group => {
+                    group.list.forEach(nav => {
+                        addRoute(nav, lang, index);
+                    });
+                });
+            } else if (nav.children) {
+                nav.children.forEach(nav => {
+                    addRoute(nav, lang, index);
+                });
+            } else {
+                addRoute(nav, lang, index);
+            }
+        });
+    });
+
+    function addRoute(page, lang, index) {
+        const component = page.path === '/changelog' ?
+            load(lang, 'changelog') :
+            loadDocs(lang, page.path);
+        let child = {
+            path: page.path.slice(1),
+            meta: {
+                title: page.title || page.name,
+                description: page.description,
+                lang
+            },
+            name: 'component-' + lang + (page.title || page.name),
+            component: component.default || component
+        };
+
+        route[index].children.push(child);
+    }
+
+    return route;
+};
+
+let route = registerRoute(navConfig);
+
+route = route.concat([{
+    path: '/',
+    redirect: defaultPath
+}, {
+    path: '*',
+    redirect: defaultPath
+}]);
+
+// export default route;
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'index',
     component: (resolve) => require(['./components/HelloWorld.vue'], resolve), //使用vue的异步组件技术 , 可以实现按需加载 .
-  },
-];
+}, ];
 
 routes.push({
-  path: '/test',
-  name: 'test',
-  component: (resolve) => require(['./docs/loading.md'], resolve),
+    path: '/test',
+    name: 'test',
+    component: (resolve) => require(['./docs/loading.md'], resolve),
 });
 
 routes.push({
-  path: '/jsx',
-  name: 'jsx',
-  component: (resolve) => require(['./components/JSX.vue'], resolve),
-  // component: () => import('./components/JSX.vue'),
+    path: '/jsx',
+    name: 'jsx',
+    component: (resolve) => require(['./components/JSX.vue'], resolve),
+    // component: () => import('./components/JSX.vue'),
 });
 
 export default new VueRouter({
-  mode: 'hash',
-  base: __dirname,
-  routes,
+    mode: 'hash',
+    base: __dirname,
+    route,
 });
